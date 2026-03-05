cf10696b0657c4699954979942aa969985a134d9
Obed-Is <obedaquino71@gmail.com>
2026-03-04 21:52:15 -0600

    Nuevo endpoint 'home' y pantalla principal

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,16 @@
 import path from 'node:path';
 import { UsersData } from '../database.js'
-import { createAccesToken, createRefreshToken } from '../helpers/token.helper.js';
+import { createAccesToken, createRefreshToken, verifyAccessToken, getPayload, verifyRefreshToken } from '../helpers/token.helper.js';
+import { createHash } from 'node:crypto';
 
 const __dirname = import.meta.dirname;
+
+const controllHome = async (req, res) => {
+    if (!req.user) return res.render(path.join(__dirname, "../views/home.ejs"), { session: false, username: null });
+
+    const { username } = req.user;
+    res.render(path.join(__dirname, "../views/home.ejs"), { session: true, username });
+}
 
 const controllRegister = (req, res) => {
     res.render(path.join(__dirname, '../views/register.ejs'));
@@ -10,30 +18,38 @@ const controllRegister = (req, res) => {
 
 const controllNewUser = async (req, res) => {
     try {
-        const { name, lastName, username, password } = req.body;
-        const userId = await UsersData.createUser(name, lastName, username, password);
+        const { name, username, password } = req.body;
 
-        if (userId === "duplicate") return res.status(409).json({ success: false, message: 'El nombre de usuario ya ha sido tomado, ingrese uno distinto' });
+        const userDuplicate = await UsersData.findUser(username);
+        if (userDuplicate)
+            return res
+                .status(409)
+                .json({ success: false, message: 'El nombre de usuario ya ha sido tomado, ingrese uno distinto' });
+
+        const hashPassword = await createHash('sha256').update(password).digest('hex');
+        const userId = await UsersData.createUser(name, username, hashPassword);
 
         const accessToken = createAccesToken(userId, username);
         const refreshToken = createRefreshToken(userId, username);
 
+        const hashRefresh = await createHash('sha256').update(refreshToken).digest('hex');
+
+        await UsersData.updateTokenUser(userId, hashRefresh);
 
         return res.status(201)
             .cookie('refreshToken', refreshToken, {
                 httpOnly: true,
-                // sameSite: 'strict',
             })
-            .cookie('accessToken', accessToken)
-            .json({ success: true, message: 'Accediendo...', accessToken });
+            .cookie('accessToken', accessToken, {
+                httpOnly: true,
+            })
+            .json({ session: true, message: 'Accediendo...' });
     } catch (error) {
-        console.log(error)
-        return res.status(500).json({ success: false, message: 'Ocurrió un error al intentar crear el usuario.', error: error });
+        return res.status(500).json({ success: false, message: 'Ocurrió un error al intentar crear el usuario.' });
     }
 };
 
 const controllLogin = (req, res) => {
-    // console.log(req);
 
     res.render(path.join(__dirname, '../views/login.ejs'), { msgFalta: null });
 };
@@ -58,6 +74,7 @@ const controllerProtected = (req, res) => {
 
 
 const userControllers = {
+    controllHome,
     controllRegister,
     controllNewUser,
     controllLogin,
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,17 +1,19 @@
 import { Router } from 'express';
 import userControllers from '../controllers/user.controller.js';
-import { validAccessToken } from '../middleware/validToken.middleware.js';
+import { sessionMiddle, validAccessToken } from '../middleware/validToken.middleware.js';
 
 const router = Router();
 
 router.get('/register', userControllers.controllRegister);
 
-router.post('/register/user', userControllers.controllNewUser)
+router.get('/home', sessionMiddle, userControllers.controllHome);
+
+router.post('/register', userControllers.controllNewUser)
 
 router.get('/login', userControllers.controllLogin);
 
 router.post('/login/user', userControllers.controllLoginUser)
 
-router.get('/protected', validAccessToken ,userControllers.controllerProtected)
+router.get('/protected', validAccessToken, userControllers.controllerProtected)
 
 export default router;
